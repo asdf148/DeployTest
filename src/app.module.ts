ec0eb4aa4d7f22e363c74ac9e69547d769681db6
asdf148 <wnduf0405_@naver.com>
2021-09-28 19:58:54 +0900

    :bug: :: Test 4
    Test 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,6 @@ import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './auth/entity/user.entity';
-import { Product } from './product/entity/product.entity';
-import { Category } from './category/entity/Category.entity';
-import { SubCategory } from './category/entity/subCategory.entity';
-import { Room } from './chat/entity/room.entity';
-import { JoinRoom } from './chat/entity/joinRoom.entity';
 
 @Module({
   imports: [
